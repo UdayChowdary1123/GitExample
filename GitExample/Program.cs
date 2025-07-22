2ea8a07a45270246c666e4050ca942f640fc52ab
UdayChowdary1123 <udaychowdary1123@gmail.com>
2025-07-22 16:07:05 +0530

    Update Program.cs

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -11,6 +11,7 @@ namespace GitExample
         static void Main(string[] args)
         {
             Console.WriteLine("Using git example");
+            Console.WriteLine("Using git example");
         }
     }
 }
